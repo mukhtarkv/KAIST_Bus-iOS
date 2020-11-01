1a5f7535ad3677c4c293b34de2291a97315b3277
mukhtar <mukhtarkussaiynbekov@gmail.com>
2020-11-01 20:12:16 +0900

    Create New Project

---
+++ KAIST_Bus-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KAIST_Bus-iOS
+//
+//  Created by Mukhtar Kussaiynbekov on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
